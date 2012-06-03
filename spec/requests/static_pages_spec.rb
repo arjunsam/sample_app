ab4a3a5992b8320667cf973352ed59946e35f64f
Arjun <arjunsam@gmail.com>
2012-06-02 18:35:00 -0700

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,18 +3,18 @@ require 'spec_helper'
 describe "Static Pages" do
   describe "Home page" do
     it "should have content 'Sample App'" do
-      visit '/static_pages/home'
+      visit root_path 
       page.should have_content('Sample App')
     end
 
     it "should have base title 'Sample App'" do
-      visit '/static_pages/home'
+      visit root_path 
       page.should have_selector('title',
                         :text=> "Sample App")        
     end
 
     it "should not have custom page title 'Home'" do
-      visit '/static_pages/home'
+      visit root_path 
       page.should_not have_selector('title',
                             :text=> "Home")
     end
@@ -22,12 +22,12 @@ describe "Static Pages" do
   
   describe "Help Page" do
     it "should have content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path 
       page.should have_content('Help')
     end
 
     it "should have title 'Help'" do
-      visit '/static_pages/help'
+      visit help_path 
       page.should have_selector('title',
                         :text=> "Help")
     end
@@ -35,12 +35,12 @@ describe "Static Pages" do
 
   describe "About Page" do
     it "should have content 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path 
       page.should have_content('About Us')
     end
 
     it "should have title 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path 
       page.should have_selector('title',
                         :text=> "About Us")
     end
@@ -48,12 +48,12 @@ describe "Static Pages" do
 
   describe "Contact Page" do
     it "should have content 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path 
       page.should have_content('Contact')
     end
  
     it "should have title 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_selector('title',
                         :text=> "Contact")
     end
